5d07c18ad78834b3607d17c1d356fd5d2ca13082
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-09 13:10:02 -0400

    Works!

--- src/Inversion.hs
+++ src/Inversion.hs
@@ -7,14 +7,14 @@ countSplitInv l r (acc,n)
   | otherwise = case (l,r) of
                   (x:xs,y:ys) -> if x > y
                                  then countSplitInv l ys (acc ++ [y], n+length l)
-                                 else countSplitInv xs r (acc ++ [x], n) 
+                                 else countSplitInv xs r (acc ++ [x], n)
 
 inversions :: (Ord a) => ([a], Int) -> ([a], Int)
 inversions (a,n)
-  | n == 1 = (a, 0)
+  | n == 1 = (a,0)
   | otherwise = (d, x+y+z)
   where
     half_length = n `div` 2
     (b,x) = inversions (fst $ splitAt half_length a, half_length)
-    (c,y) = inversions (snd $ splitAt half_length a, half_length)
+    (c,y) = inversions (snd $ splitAt half_length a, n - half_length)
     (d,z) = countSplitInv b c ([],0)
--- test/InversionSpec.hs
+++ test/InversionSpec.hs
@@ -3,7 +3,7 @@ module InversionSpec (main, spec) where
 import Test.Hspec
 import Test.QuickCheck
 
-import Inversion 
+import Inversion
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -14,4 +14,6 @@ spec :: Spec
 spec = do
   describe "inversions" $ do
     it "returns sorted list and n choose k" $ do
-      inversions ([3,2,1], 3) `shouldBe` ([1,2,3], 3) 
+      inversions ([2,1], 2) `shouldBe` ([1,2], 1)
+      inversions ([3,2,1], 3) `shouldBe` ([1,2,3], 3)
+      inversions ([6,5,4,3,2,1], 6) `shouldBe` ([1,2,3,4,5,6], 15)
