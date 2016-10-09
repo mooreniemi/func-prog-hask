2296a31fe13bf7498cff6704a983fbc632653cfc
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-09 14:32:27 -0400

    remove length call which was extra traversal

--- src/Inversion.hs
+++ src/Inversion.hs
@@ -1,20 +1,21 @@
 module Inversion where
 -- https://www.coursera.org/learn/algorithm-design-analysis/lecture/GFmmJ/o-n-log-n-algorithm-for-counting-inversions-i
 
-countSplitInv :: (Ord a) => [a] -> [a] -> ([a], Int) -> ([a], Int)
-countSplitInv l r (acc,n)
+countSplitInv :: (Ord a) => [a] -> Int -> [a] -> ([a], Int) -> ([a], Int)
+countSplitInv l left_length r (acc,n)
   | null l || null r = (acc ++ l ++ r, n)
   | otherwise = case (l,r) of
                   (x:xs,y:ys) -> if x > y
-                                 then countSplitInv l ys (acc ++ [y], n+length l)
-                                 else countSplitInv xs r (acc ++ [x], n)
+                                 then countSplitInv l left_length ys (acc ++ [y], n+left_length)
+                                 else countSplitInv xs left_length r (acc ++ [x], n)
 
 inversions :: (Ord a) => ([a], Int) -> ([a], Int)
 inversions (a,n)
   | n == 1 = (a,0)
   | otherwise = (d, x+y+z)
   where
-    half_length = n `div` 2
-    (b,x) = inversions (fst $ splitAt half_length a, half_length)
-    (c,y) = inversions (snd $ splitAt half_length a, n - half_length)
-    (d,z) = countSplitInv b c ([],0)
+    left_length = n `div` 2
+    right_length = n - (n `div` 2)
+    (b,x) = inversions (fst $ splitAt left_length a, left_length)
+    (c,y) = inversions (snd $ splitAt left_length a, right_length)
+    (d,z) = countSplitInv b left_length c ([],0)
