b6bac58dd51f3a0d3bd90a1881bc92ce87cb14ef
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-10 19:24:02 -0400

    using sequence

--- src/Inversion.hs
+++ src/Inversion.hs
@@ -1,23 +1,30 @@
-module Inversion where
 -- https://www.coursera.org/learn/algorithm-design-analysis/lecture/GFmmJ/o-n-log-n-algorithm-for-counting-inversions-i
+module Inversion where
+import Data.Sequence as S
+import Data.Foldable as F
 
-countSplitInv :: (Ord a) => [a] -> Int -> [a] -> ([a], Int) -> ([a], Int)
-countSplitInv l left_length r (acc,n)
-  | null l || null r = ((reverse acc) ++ l ++ r, n)
-  | otherwise = case (l,r) of
-                  (x:xs,y:ys) -> if x > y
-                                 then countSplitInv l left_length ys (y:acc, n+left_length)
-                                 else countSplitInv xs (left_length-1) r (x:acc, n)
+countSplitInv :: (Ord a) => S.Seq a -> S.Seq a -> (S.Seq a, Int) -> (S.Seq a, Int)
+countSplitInv l r (acc,n)
+  | otherwise = case (viewl l, viewl r) of
+                  (EmptyL,_) -> (acc >< r, n)
+                  (_,EmptyL) -> (acc >< l, n)
+                  (x:<xs,y:<ys) -> if x > y
+                                 then countSplitInv l ys (acc|>y, n + S.length l)
+                                 else countSplitInv xs r (acc|>x, n)
 
-inversions :: (Ord a) => ([a], Int) -> ([a], Int)
-inversions ([], 0) = ([], 0)
-inversions (a,n)
-  | n == 1 = (a,0)
+inversions' :: (Ord a) => S.Seq a -> (S.Seq a, Int)
+inversions' a
+  | n <= 1 = (a,0)
   | otherwise = (d, x+y+z)
   where
+    n = S.length a
     left_length = n `div` 2
     right_length = n - left_length
-    (l,r) = splitAt left_length a
-    (b,x) = inversions (l, left_length)
-    (c,y) = inversions (r, right_length)
-    (d,z) = countSplitInv b left_length c ([],0)
+    (l,r) = S.splitAt left_length a
+    (b,x) = inversions' l
+    (c,y) = inversions' r
+    (d,z) = countSplitInv b c (S.empty,0)
+
+inversions :: (Ord a) => ([a], Int) -> ([a], Int)
+inversions (a,_) = let (list,count) = inversions' $ S.fromList a
+                       in (F.toList list, count)
