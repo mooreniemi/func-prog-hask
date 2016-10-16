5e84585532f71e8bcd0cd142e5aec9c3ea961f13
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-10-16 14:23:57 -0400

    partitioning works

--- src/QuickSort.hs
+++ src/QuickSort.hs
@@ -1,34 +1,33 @@
+{-# LANGUAGE ViewPatterns, PatternSynonyms #-}
 module QuickSort where
-import Data.Sequence as S
-import Data.Foldable as F
+import qualified Data.Sequence as S
+import Data.Sequence (ViewR(..),ViewL(..),(|>),(><),(<|))
+import Control.Arrow as A (first)
+import Debug.Trace
 
 -- moves pivot element into correct position
-partition' :: (Ord a) => S.Seq a -> S.Seq a
-partition' seq = (firstHalf|>pivot)><secondHalf
-  where
-    pivot = S.index seq 0
-    (_, position) = partitioner seq 0 0
-    (_, rest) = S.splitAt 1 seq
-    (firstHalf, secondHalf) = S.splitAt position rest
+partition' :: (Show a, Ord a) => S.Seq a -> S.Seq a
+partition' seq = case first S.viewl (partitioner seq 0 1) of
+                   (pivot:<s,position) -> (firstHalf|>pivot)><secondHalf
+                     where
+                       (firstHalf,secondHalf) = S.splitAt position s
 
 -- tells you where the pivot element's final position should be
-partitioner :: (Ord a) => S.Seq a -> Int -> Int -> (S.Seq a, Int)
+partitioner :: (Show a, Ord a) => S.Seq a -> Int -> Int -> (S.Seq a, Int)
 partitioner a i j
-  | n <= 1 = (a, 0)
-  | j > n = (a, i-2)
-  | otherwise = case (viewl a) of
-                (p:<ps) -> case (viewl ps) of
-                           (x:<xs) -> partitioner a partition_boundary seen_boundary
-                                      where
-                                        seen_boundary = j+1
-                                        partition_boundary = if p>x
-                                                             then i+1
-                                                             else i
-  where
-    n = S.length a
-
-quicksort :: (Ord a) => S.Seq a -> S.Seq a
-quicksort a
-  | n <= 1 = a
+  | n <= 1 || j > n = (a, i)
+  | otherwise = case (S.viewl a) of
+                (p:<ps) -> let (seen, unseen) = S.splitAt j ps
+                           in case S.viewr seen of
+                                (xs:>x) -> partitioner b partition_boundary seen_boundary
+                                  where
+                                    seen_boundary = j+1
+                                    partition_boundary = if x<p
+                                                         then i+1
+                                                         else i
+                                    b = if x<p
+                                        then ((p<|(x<|xs))><unseen)
+                                        else ((p<|(xs|>x))><unseen)
+                                (EmptyR) -> (a, i)
   where
     n = S.length a
--- test/QuickSortSpec.hs
+++ test/QuickSortSpec.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module QuickSortSpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
 
 import QuickSort
-import Data.Sequence as S
+import qualified Data.Sequence as S
+import Data.List (sort, elemIndex)
+import Data.Maybe (fromJust)
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -16,12 +19,21 @@ spec = do
   describe "partition" $ do
     it "single element is vacuously parititioned" $ do
       partition'(S.fromList([3])) `shouldBe` S.fromList([3])
-    it "returns seq when seen boundary exceeds length" $ do
+    it "double element sorts" $ do
       partition'(S.fromList([3,2])) `shouldBe` S.fromList([2,3])
+      partition'(S.fromList([2,3])) `shouldBe` S.fromList([2,3])
     it "otherwise continues scanning" $ do
       partition'(S.fromList([3,2,1])) `shouldBe` S.fromList([2,1,3])
-      partition'(S.fromList([3,4,2,1])) `shouldBe` S.fromList([2,1,3,4])
-  describe "quicksort" $ do
-    it "handles empty sequences" $ do
-      let emptyIntSeq = S.fromList([] :: [Integer])
-      quicksort emptyIntSeq `shouldBe` emptyIntSeq
+      partition'(S.fromList([3,4,2,1])) `shouldBe` S.fromList([1,2,3,4])
+    it "should place pivot element at its sorted position" $ property $
+      \(NonEmpty s@((x :: Int):_)) -> let sorted = sort s
+                                          correct_position = elemIndex x sorted
+                                          partitioned_index = S.elemIndexL x (partition'(S.fromList s))
+                                      in
+                                        partitioned_index === correct_position
+    it "all left of pivot smaller than pivot, all right larger than pivot" $ property $
+      \(NonEmpty s@((x :: Int):_)) -> let partitioned_seq = partition'(S.fromList s)
+                                          partitioned_index = fromJust $ S.elemIndexL x (partitioned_seq)
+                                          (firstHalf,secondHalf) = S.splitAt partitioned_index partitioned_seq
+                                      in
+                                        all (< x) firstHalf && all (>= x) secondHalf
